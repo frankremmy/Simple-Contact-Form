8c39b7c28d82149ef10233af1422d6a010bb3ba7
frankremmy <princeugo.remix@gmail.com>
2024-10-10 11:52:15 +0100

    Add activation fuction and admin notice

--- simple-contact-form.php
+++ simple-contact-form.php
@@ -10,3 +10,31 @@
  * Author URI:        https://frankremmy.com/
  * Text Domain:       simple-contact-form
  */
+
+//  Check if ABSPATH is define, and exit if not
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+// Function to run when the plugin is activated
+function scf_activate_plugin(){
+    // Add transient to show activation notice in the admin area
+    set_transient('scf_activation_notice', true, 5);
+}
+// Hook the activation function to 'register_activation_hook'
+register_activation_hook(__FILE__, 'scf_activate_plugin');
+
+// Function to display the activation notice
+function scf_display_activation_notice(){
+    // Check if transient is set
+    if (get_transient('scf_activation_notice')){
+        ?>  <div class='notice notice-success is-dismissible'>
+            <p><?php _e('Simple Contact Form plugin activated successfully!', 'scf-plugin'); ?></p>
+            </div>
+        <?php
+        // Delete transient show it only show once
+        delete_transient('scf_activation_notice');
+    }   
+}
+// Hook it into 'admin_notices'
+add_action('admin_notices', 'scf_display_activation_notice');
