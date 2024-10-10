17d1fb535c57ee4dad47a6f6f83d2ad4429e3b6d
frankremmy <princeugo.remix@gmail.com>
2024-10-10 06:08:37 +0100

    Initial commit - basic plugin setup

---
+++ simple-contact-form.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+ * Plugin Name:       Simple Contact Form
+ * Plugin URI:        https://frankremmy.com
+ * Description:       A WordPress plugin that lets users add a contact form to any page or post using a shortcode. The form will collect basic information like name, email, and message, and also store the data in the database as well as send an email notification.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Frank Remmy
+ * Author URI:        https://frankremmy.com/
+ * Text Domain:       simple-contact-form
+ */
