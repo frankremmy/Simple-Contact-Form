3d75984490b3f9de17cbe9c7415a2017a18a6f2f
frankremmy <princeugo.remix@gmail.com>
2024-10-11 01:37:42 +0100

    Add basic HTML form for the contact form shortcode

--- simple-contact-form.php
+++ simple-contact-form.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Simple Contact Form
  * Plugin URI:        https://frankremmy.com
  * Description:       A WordPress plugin that lets users add a contact form to any page or post using a shortcode. The form will collect basic information like name, email, and message, and also store the data in the database as well as send an email notification.
- * Version:           1.0
+ * Version:           0.1
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Frank Remmy
@@ -38,3 +38,35 @@ function scf_display_activation_notice(){
 }
 // Hook it into 'admin_notices'
 add_action('admin_notices', 'scf_display_activation_notice');
+
+// Display a simple message with the shortcode
+function scf_display_contact_form(){
+    $content = '';
+
+    $content .= '<form method="post" action="' . esc_url($_SERVER['REQUEST_URI']) . '">';
+    $content .= '<p>';
+    $content .= 'Name (required) <br />';
+    $content .= '<input type="text" name="scf-name" pattern="[a-zA-Z0-9 ]+" value="' . (isset($_POST["scf-name"]) ? esc_attr($_POST["scf-name"]) : '') . '" size="80" />';
+    $content .= '</p>';
+    $content .= '<p>';
+    $content .= 'Email (required) <br />';
+    $content .= '<input type="email" name="scf-email" value="' . (isset($_POST["scf-email"]) ? esc_attr($_POST["scf-email"]) : '') . '" size="80" />';
+    $content .= '</p>';
+    $content .= '<p>';
+    $content .= 'Message (required) <br />';
+    $content .= '<textarea name="scf-message" rows="10" cols="65">' . (isset($_POST["scf-message"]) ? esc_attr($_POST["scf-message"]) : '') . '</textarea>';
+    $content .= '</p>';
+    $content .= '<p><input type="submit" name="scf-submitted" value="Send"/></p>';
+    $content .= '</form>';
+
+    return $content;
+}
+
+// Register the shortcode
+function scf_register_shortcodes(){
+    add_shortcode('simple_contact_form', 'scf_display_contact_form');
+}
+add_action( 'init', 'scf_register_shortcodes');
+
+// Commit 1
+
