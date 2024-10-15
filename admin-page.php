e46b14192f00229870cd4e86af9488a78d1b23c2
frankremmy <princeugo.remix@gmail.com>
2024-10-15 14:50:47 +0100

    Add admin page to display form submissions

---
+++ admin-page.php
@@ -0,0 +1,41 @@
+<?php
+
+function scf_add_admin_menu(){
+    add_menu_page(
+        'Simple Contact Form Submissions', //Page titel
+        'SCF Submissions', // Menu title
+        'scf_submissions', //Menu slug
+        'scf_display_submissions' //callback function
+    );
+}
+add_action( 'admin_menu', 'scf_add_admin_menu');
+
+// Display form submission on admin page
+function scf_display_submissions(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'scf_submissions';
+    $results = $wpdb->get_results("SELECT * FROM $table_name"); //Retrieve submissions from db
+    
+    //Display submissions
+    echo '<div class="wrap">';
+    echo '<h1>Simple Contact For Submissions</h1>';
+    echo '<table class="widefat fixed" cellspacing="0">';
+    echo '<thead><tr><th>Name</th><th>Email</th><th>Message</th><th>Date</th></tr></thead>';
+    echo '<tbody>';
+    if ($results){
+        foreach ($results as $submissions) {
+            echo '<tr>';
+            echo '<td>' . esc_html($submissions->name) . '</td>';
+            echo '<td>' . esc_html($submissions->email) . '</td>';
+            echo '<td>' . esc_html($submissions->message) . '</td>';
+            echo '<td>' . esc_html($submissions->submitted_at) . '</td>';
+            echo '</tr>';
+        }
+    } else {
+        echo '<tr><td colspan="4">No submissions found.</td></tr>';
+    }
+    
+    echo '</tbody>';
+    echo '</table>';
+    echo '</div>';
+}
